a2544835df5f4b7a2b1b2a39f29914f5527d6b46
Jevin Sweval <jevinsweval@gmail.com>
2025-02-22 18:23:58 -0500

    here comes the firehose

--- tools/applespi-log/applespi-log.c
+++ tools/applespi-log/applespi-log.c
@@ -44,12 +44,11 @@ int main(void) {
     xpc_connection_activate(xpc_con);
     xpc_object_t req = xpc_dictionary_create(NULL, NULL, 0);
     xpc_dictionary_set_uint64(req, "action", 3);
-    xpc_object_t u64_4_obj    = xpc_uint64_create(0x4);
-    const char *keys[]        = {"foo"};
-    const xpc_object_t vals[] = {u64_4_obj};
-    xpc_object_t con_send_obj = xpc_dictionary_create(keys, vals, 1);
+    xpc_dictionary_set_uint64(req, "flags", 3);
+    xpc_dictionary_set_uint64(req, "types", 0x8 | 0x4 | 0x2 | 0x1);
+
     printf("con_send_obj obj: %p desc: '%s'\n", req, xpc_copy_description(req));
-    xpc_connection_send_message_with_reply(xpc_con, con_send_obj, queue, ^(xpc_object_t o) { event_handler(o); });
+    xpc_connection_send_message_with_reply(xpc_con, req, queue, ^(xpc_object_t o) { event_handler(o); });
     dispatch_main();
     printf("post dispatch_main()\n");
     fflush(stdout);
