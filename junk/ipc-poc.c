ba0b6e7120d46f31a497315aa93e8e065ddcedb7
Jevin Sweval <jevinsweval@gmail.com>
2025-03-06 01:45:49 -0500

    wip

--- junk/ipc-poc.c
+++ junk/ipc-poc.c
@@ -1,5 +1,12 @@
+#undef NDEBUG
+#include <assert.h>
+
 #include <CoreFoundation/CoreFoundation.h>
 #include <mach/mach.h>
+#include <mach/mach_init.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define plog(str, args...) printf("[%6d] " str "\n", getpid(), ##args)
 #define HandleError(kr)                                                                  \
@@ -9,8 +16,14 @@
         exit(1);                                                                         \
     }
 
+struct port_msg {
+    mach_msg_header_t head;
+    mach_msg_body_t body;
+    mach_msg_port_descriptor_t port;
+};
+
 struct favorite_animal_msg {
-    mach_msg_header_t header;
+    mach_msg_header_t head;
     mach_msg_size_t count;              // will be 1
     mach_msg_ool_descriptor_t question; // is type MACH_MSG_OOL_DESCRIPTOR
 };
@@ -37,16 +50,16 @@ void send_message(mach_port_t remotePort) {
 
     struct favorite_animal_msg msg = {};
 
-    mach_port_t localPort      = create_port(); // 1
-    msg.header.msgh_bits       = MACH_MSGH_BITS_SET(MACH_MSG_TYPE_MOVE_SEND_ONCE,
-                                                    MACH_MSG_TYPE_MAKE_SEND_ONCE, // 2
-                                                    0, MACH_MSGH_BITS_COMPLEX);
-    msg.header.msgh_local_port = localPort; // 3
+    mach_port_t localPort    = create_port(); // 1
+    msg.head.msgh_bits       = MACH_MSGH_BITS_SET(MACH_MSG_TYPE_MOVE_SEND_ONCE,
+                                                  MACH_MSG_TYPE_MAKE_SEND_ONCE, // 2
+                                                  0, MACH_MSGH_BITS_COMPLEX);
+    msg.head.msgh_local_port = localPort; // 3
 
-    msg.header.msgh_id           = 12345;
-    msg.header.msgh_remote_port  = sendPort;
-    msg.header.msgh_size         = sizeof(msg);
-    msg.header.msgh_voucher_port = MACH_PORT_NULL;
+    msg.head.msgh_id           = 12345;
+    msg.head.msgh_remote_port  = sendPort;
+    msg.head.msgh_size         = sizeof(msg);
+    msg.head.msgh_voucher_port = MACH_PORT_NULL;
 
     char questionStr[1024] = {};
     snprintf(questionStr, 1024, "Yo, what's your favorite animal? [Sent from %d]", getpid());
@@ -59,7 +72,7 @@ void send_message(mach_port_t remotePort) {
     msg.question.deallocate = 0;
 
     plog("Sending message from child");
-    HandleError(mach_msg(&msg.header,
+    HandleError(mach_msg(&msg.head,
                          MACH_RCV_MSG | MACH_SEND_MSG, // 4
                          sizeof(struct favorite_animal_msg),
                          sizeof(struct favorite_animal_msg) + MAX_TRAILER_SIZE, localPort, 0000000,
@@ -70,10 +83,10 @@ void send_message(mach_port_t remotePort) {
 
 void recv_message(mach_port_t localPort) {
     struct favorite_animal_msg msg = {};
-    msg.header.msgh_local_port     = localPort;
-    msg.header.msgh_size           = sizeof(msg) + MAX_TRAILER_SIZE;
-    HandleError(mach_msg_receive(&msg.header));
-    plog("msgh_id: %d", msg.header.msgh_id);
+    msg.head.msgh_local_port       = localPort;
+    msg.head.msgh_size             = sizeof(msg) + MAX_TRAILER_SIZE;
+    HandleError(mach_msg_receive(&msg.head));
+    plog("msgh_id: %d", msg.head.msgh_id);
     plog("%s", (char *)msg.question.address);
 
     char respStr[1024];
@@ -82,22 +95,103 @@ void recv_message(mach_port_t localPort) {
     msg.question.size    = (mach_msg_size_t)strlen(respStr);
     msg.question.copy    = MACH_MSG_PHYSICAL_COPY;
 
-    msg.header.msgh_local_port = MACH_PORT_NULL;
-    HandleError(mach_msg_send(&msg.header));
+    msg.head.msgh_local_port = MACH_PORT_NULL;
+    HandleError(mach_msg_send(&msg.head));
 }
 
 int main() {
-    mach_port_t port = create_port();
-    pid_t pid        = fork();
+    // mach_port_t port = create_port();
+    int kr;
+    mach_port_t fake_bsp = MACH_PORT_NULL;
+    kr                   = mach_port_allocate(mach_task_self(), MACH_PORT_RIGHT_RECEIVE, &fake_bsp);
+    if (kr != KERN_SUCCESS) {
+        mach_error("mach_port_allocate", kr);
+        abort();
+    }
+    kr = mach_port_insert_right(mach_task_self(), fake_bsp, fake_bsp, MACH_MSG_TYPE_MAKE_SEND);
+    if (kr != KERN_SUCCESS) {
+        mach_error("mach_port_insert_right", kr);
+        abort();
+    }
+    kr = task_set_bootstrap_port(mach_task_self(), fake_bsp);
+    if (kr != KERN_SUCCESS) {
+        mach_error("task_set_bootstrap_port", kr);
+        abort();
+    }
+    pid_t pid = fork();
     if (pid == 0) {
         plog("I am the child");
-        sleep(2);
-        send_message(port);
+        sleep(1);
+        kr = task_get_bootstrap_port(mach_task_self(), &fake_bsp);
+        if (kr != KERN_SUCCESS) {
+            mach_error("failed to get parent bootstrap port", kr);
+            abort();
+        }
+        task_t child_self_task_kernel_port = MACH_PORT_NULL;
+        kr =
+            task_get_special_port(mach_task_self(), TASK_KERNEL_PORT, &child_self_task_kernel_port);
+        if (KERN_SUCCESS != kr) {
+            mach_error("child task_get_special_port", kr);
+            abort();
+        }
+        struct port_msg pmsg = {.head =
+                                    {
+                                        .msgh_bits = MACH_MSGH_BITS(MACH_MSG_TYPE_COPY_SEND, 0) |
+                                                     MACH_MSGH_BITS_COMPLEX,
+                                        .msgh_size        = sizeof(struct port_msg),
+                                        .msgh_remote_port = fake_bsp,
+                                        .msgh_local_port  = MACH_PORT_NULL,
+                                    },
+                                .body =
+                                    {
+                                        .msgh_descriptor_count = 1,
+                                    },
+                                .port = {
+                                    .name        = child_self_task_kernel_port,
+                                    .disposition = MACH_MSG_TYPE_COPY_SEND,
+                                    .type        = MACH_MSG_PORT_DESCRIPTOR,
+                                }};
+        printf("child calling mach_msg_send(&pmsg.head)\n");
+        fflush(stdout);
+        kr = mach_msg_send(&pmsg.head);
+        printf("child MACH_PORT_VALID(fake_bsp): %d\n", MACH_PORT_VALID(fake_bsp));
+        printf("child MACH_PORT_VALID(child_self_task_kernel_port): %d\n",
+               MACH_PORT_VALID(child_self_task_kernel_port));
+        if (kr != KERN_SUCCESS) {
+            mach_error("child mach_msg_send(&pmsg.header)", kr);
+            abort();
+        }
+        // mach_port_deallocate(mach_task_self(), child_self_task_kernel_port);
+        sleep(1);
+        printf("child done sleeping one, sent its port, sleeping for 5 seconds\n");
+        fflush(stdout);
+        // send_message(port);
     } else if (pid > 0) {
         plog("I am the parent");
-        sleep(2);
+        printf("parent MACH_PORT_VALID(bootstrap_port): %d port: 0x%08x\n",
+               MACH_PORT_VALID(bootstrap_port), bootstrap_port);
+        kr = task_set_bootstrap_port(mach_task_self(), bootstrap_port);
+        if (kr != KERN_SUCCESS) {
+            mach_error("post fork parent task_set_bootstrap_port", kr);
+            abort();
+        }
+        sleep(3);
+        struct port_msg parent_pmsg;
+        memset(&parent_pmsg, 0, sizeof(parent_pmsg));
 
-        recv_message(port);
+        kr = mach_msg(&parent_pmsg.head, MACH_RCV_MSG, 0,
+                      sizeof(struct port_msg) + sizeof(mach_msg_trailer_t), fake_bsp,
+                      MACH_MSG_TIMEOUT_NONE, MACH_PORT_NULL);
+        printf("parent MACH_PORT_VALID(fake_bsp) post mach_msg: %d\n", MACH_PORT_VALID(fake_bsp));
+        if (kr != KERN_SUCCESS) {
+            mach_error("post fork parent mach_msg get port", kr);
+            abort();
+        }
+        mach_port_t parents_child_port = parent_pmsg.port.name;
+        printf("parent MACH_PORT_VALID(parents_child_port): %d port: 0x%08x\n",
+               MACH_PORT_VALID(parents_child_port), parents_child_port);
+        assert(MACH_PORT_VALID(parents_child_port));
+        // recv_message(port);
         wait(&pid);
     }
 
